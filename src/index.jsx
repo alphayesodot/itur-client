90457267786f38cc511318d349eaef3dabb95be3
כרמל צדוק <carmelcarmel88888@gmail.com>
2021-02-11 19:24:34 +0200

    Add shared snackbars

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './i18n';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { SnackbarProvider } from 'notistack';
 import theme from './theme';
 import './index.css';
 import App from './App';
@@ -9,7 +10,9 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <SnackbarProvider maxSnack={3} preventDuplicate>
+        <App />
+      </SnackbarProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
