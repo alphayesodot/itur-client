d7e52c8f1cf8e130b2b67923065b602a93ed3841
adarpascal <adarpascal100@gmail.com>
2021-04-08 05:29:31 +0300

    Fix: eslint settings + config server url comes from config

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -7,7 +7,7 @@ const headers = {
 
 class ConfigService {
   static async setConfigVariables() {
-    const { data } = await axios.get('/config', { ...headers });
+    const { data } = await axios.get(config.configServerUri, { ...headers });
     config.uri = { ...data.uri };
     config.token_name = data.token_name;
     config.secret = data.secret;
