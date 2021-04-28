ffb0c980d71b712a1d6c2393cbff2564191ddcf6
lustigy96 <44235562+lustigy96@users.noreply.github.com>
2021-04-28 15:35:11 +0300

    Update src/pages/XmlUpload/index.styles.js
    Co-authored-by: carmeltsadok <69649728+carmeltsadok@users.noreply.github.com>

--- src/pages/XmlUpload/index.styles.js
+++ src/pages/XmlUpload/index.styles.js
@@ -1,11 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
-  // root: {
-  //   width: '100%',
-  //   textAlign: 'center',
-  //   alignItems: 'center',
-  // },
   content: {
     display: 'inline-flex',
     flexWrap: 'wrap',
