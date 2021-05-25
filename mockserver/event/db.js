bc0a5b486f58155e8ae247af6fd3f1859e896f9c
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-05-25 13:18:28 +0300

    fix: Fix data

--- mockserver/event/db.js
+++ mockserver/event/db.js
@@ -1,85 +1,85 @@
 const events = [
-  // {
-//   id: '1',
-//   node: {
-//     id: '123',
-//     name: 'ראיון',
-//   },
-//   malshabShort: {
-//     identityNumber: '22222222',
-//     firstName: 'נועה',
-//     lastName: 'קירל',
-//   },
-//   time: '2021-05-09T06:00:07.996+00:00',
-//   location: 'place',
-//   interviewersIds: ['3', '6', '5'],
-//   url: 'http://',
-// },
-// {
-//   id: '2',
-//   node: {
-//     id: '123',
-//     name: 'ראיון',
-//   },
-//   malshabShort: {
-//     identityNumber: '9999999',
-//     firstName: 'דנה',
-//     lastName: 'זרמון',
-//   },
-//   time: '2021-05-09T07:00:07.996+00:00',
-//   location: 'place',
-//   interviewersIds: ['3', '6'],
-//   url: 'http://',
-// },
-// {
-//   id: '3',
-//   node: {
-//     id: '666',
-//     name: 'ראיון',
-//   },
-//   malshabShort: {
-//     identityNumber: '9999999',
-//     firstName: 'דנה',
-//     lastName: 'זרמון',
-//   },
-//   time: '2021-05-10T12:00:07.996+00:00',
-//   location: 'place',
-//   interviewersIds: ['4', '7', '2'],
-//   url: 'http://',
-// },
-// {
-//   id: '4',
-//   node: {
-//     id: '123',
-//     name: 'ראיון',
-//   },
-//   malshabShort: {
-//     identityNumber: '55555555',
-//     firstName: 'ישראל',
-//     lastName: 'ישראלי',
-//   },
-//   time: '2021-05-09T14:00:07.996+00:00',
-//   location: 'place',
-//   interviewersIds: ['5', '12', '9'],
-//   url: 'http://',
-// },
-// {
-//   id: '5',
-//   node: {
-//     id: '123',
-//     name: 'ראיון',
-//   },
-//   malshabShort: {
-//     identityNumber: '3333333',
-//     firstName: 'עדי',
-//     lastName: 'ביטי',
-//   },
-//   time: '2021-05-09T10:00:07.996+00:00',
-//   location: 'place',
-//   interviewersIds: ['5', '12', '9'],
-//   url: 'http://',
-//   results: { notes: [] },
-// },
+  {
+    id: '1',
+    node: {
+      id: '123',
+      name: 'ראיון',
+    },
+    malshabShort: {
+      identityNumber: '22222222',
+      firstName: 'נועה',
+      lastName: 'קירל',
+    },
+    time: '2021-05-09T06:00:07.996+00:00',
+    location: 'place',
+    interviewersIds: ['507f1f77bcf86cd799439014', '507f1f77bcf86cd799439013', '507f1f77bcf86cd799439012'],
+    url: 'http://',
+  },
+  {
+    id: '2',
+    node: {
+      id: '123',
+      name: 'ראיון',
+    },
+    malshabShort: {
+      identityNumber: '9999999',
+      firstName: 'דנה',
+      lastName: 'זרמון',
+    },
+    time: '2021-05-09T07:00:07.996+00:00',
+    location: 'place',
+    interviewersIds: ['507f1f77bcf86cd799439014', '507f1f77bcf86cd799439013'],
+    url: 'http://',
+  },
+  {
+    id: '507f1f77bcf86cd799439014',
+    node: {
+      id: '666',
+      name: 'ראיון',
+    },
+    malshabShort: {
+      identityNumber: '9999999',
+      firstName: 'דנה',
+      lastName: 'זרמון',
+    },
+    time: '2021-05-10T12:00:07.996+00:00',
+    location: 'place',
+    interviewersIds: ['507f1f77bcf86cd799439011'],
+    url: 'http://',
+  },
+  {
+    id: '4',
+    node: {
+      id: '123',
+      name: 'ראיון',
+    },
+    malshabShort: {
+      identityNumber: '55555555',
+      firstName: 'ישראל',
+      lastName: 'ישראלי',
+    },
+    time: '2021-05-09T14:00:07.996+00:00',
+    location: 'place',
+    interviewersIds: ['507f1f77bcf86cd799439012', '507f1f77bcf86cd799439016', '507f1f77bcf86cd799439015'],
+    url: 'http://',
+  },
+  {
+    id: '5',
+    node: {
+      id: '123',
+      name: 'ראיון',
+    },
+    malshabShort: {
+      identityNumber: '3333333',
+      firstName: 'עדי',
+      lastName: 'ביטי',
+    },
+    time: '2021-05-09T10:00:07.996+00:00',
+    location: 'place',
+    interviewersIds: ['507f1f77bcf86cd799439012', '507f1f77bcf86cd799439016', '507f1f77bcf86cd799439015'],
+    url: 'http://',
+    results: { notes: [] },
+  },
   {
     id: '507f1f77bcf86ca7994390555',
     node: {
--- mockserver/nodeGroup/db.js
+++ mockserver/nodeGroup/db.js
@@ -2,7 +2,13 @@ const nodeGroups = [{
   id: '507f1f77bcf86cd7994390555',
   name: 'מסלול1',
   unitId: '507f191e810c19729de861ea',
-  usersIds: ['507f1f77bcf86cd799439011'],
+  usersIds: ['507f1f77bcf86cd799439011', '507f1f77bcf86cd799439014', '507f1f77bcf86cd799439016'],
+},
+{
+  id: '507f1f77bcf86cd7994390556',
+  name: 'מסלול2',
+  unitId: '507f191e810c19729de861ea',
+  usersIds: ['507f1f77bcf86cd799439012', '507f1f77bcf86cd799439013', '507f1f77bcf86cd799439015'],
 }];
 
 export default nodeGroups;
