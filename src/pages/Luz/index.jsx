c4be4484ea81985af30fe2f3634e708e1cd7e8ee
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-05-31 17:06:12 +0300

    feat: Add new page

---
+++ src/pages/Luz/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import useStyles from './index.styles';
+
+const Luz = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} />
+  );
+};
+
+export default Luz;
