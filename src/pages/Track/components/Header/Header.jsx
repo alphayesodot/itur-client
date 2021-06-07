5c05f4869378442177f3a1b9fded6f413ec39452
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-06-07 10:16:13 +0300

    refactor: Remove useless code

--- src/pages/Track/components/Header/Header.jsx
+++ src/pages/Track/components/Header/Header.jsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { Typography } from '@material-ui/core';
-import { observer } from 'mobx-react-lite';
 import DashboardCard from '../../../../common/DashboardCard/DashboardCard';
 import ScheduleStore from '../../../../stores/Schedule.store';
 import useStyles from './Header.styles';
 import NodeGroupSelect from '../../../../common/NodeGroupSelect/NodeGroupSelect';
 import DateInput from '../../../../common/DateInput/DateInput';
 
-const Header = observer(({
+const Header = ({
   unit,
   selectedNodeGroup,
   setSelectedNodeGroup,
@@ -35,22 +34,20 @@ const Header = observer(({
   return (
     <DashboardCard className={classes.root}>
       <div className={classes.content}>
-        <div className={classes.main}>
-          <DateInput selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
-          <NodeGroupSelect
-            selectedNodeGroup={selectedNodeGroup}
-            setSelectedNodeGroup={setSelectedNodeGroup}
-          />
-          <Typography className={`${classes.unit} ${classes.item}`}>
-            :
-            {t('title.unit')}
-            {' '}
-            <strong>{unit ? unit.name : ''}</strong>
-          </Typography>
-        </div>
+        <DateInput selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+        <NodeGroupSelect
+          selectedNodeGroup={selectedNodeGroup}
+          setSelectedNodeGroup={setSelectedNodeGroup}
+        />
+        <Typography className={`${classes.unit} ${classes.item}`}>
+          :
+          {t('title.unit')}
+          {' '}
+          <strong>{unit ? unit.name : ''}</strong>
+        </Typography>
       </div>
     </DashboardCard>
   );
-});
+};
 
 export default Header;
