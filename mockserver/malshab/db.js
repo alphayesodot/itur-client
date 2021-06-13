5fe1a247962e64408043ecca7f80973206dc9608
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-06-13 11:39:17 +0300

    fix: Remove data

--- mockserver/malshab/db.js
+++ mockserver/malshab/db.js
@@ -1,29 +1,3 @@
-const malshabs = [
-  {
-    identityNumber: '0',
-    personalNumber: '0',
-    firstName: 'נועה',
-    lastName: 'קירל',
-    gender: 0,
-    birthDate: '2003-05-09T00:00:07.996+00:00',
-    medicalProfile: 82,
-    kaba: 56,
-    dapar: 90,
-    addresses: [{
-      cityId: 1,
-      cityName: 'ראשון לציון',
-      houseNumber: 12,
-      street: 'הרצל',
-    }],
-    birthCountry: 'רוסיה',
-    imigrationDate: '2005-05-13T00:00:07.996+00:00',
-    hasIsraeliCitizenship: true,
-    hasAnotherCitizenship: false,
-    isBereaved: false,
-    schoolId: 32,
-    schoolName: 'בן גוריון',
-    email: 'noa@gmail.com',
-  },
-];
+const malshabs = [];
 
 export default malshabs;
