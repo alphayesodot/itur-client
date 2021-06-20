2a1811b663483f08d03f585e59db39d621766edc
adarpascal <adarpascal100@gmail.com>
2021-06-21 01:26:38 +0300

    feat: embed changed to iframe and permission gain

--- src/pages/InterviewDashboard/components/Malshab/Malshab.jsx
+++ src/pages/InterviewDashboard/components/Malshab/Malshab.jsx
@@ -5,12 +5,18 @@ import MalshabToolbar from './MalshabToolbar/MalshabToolbar';
 
 const Malshab = ({ malshab, event }) => {
   const classes = useStyles();
-
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    .then((stream) => {
+      /* use the stream */
+    })
+    .catch((err) => {
+      /* handle the error */
+    });
   return (
     <DashboardCard className={classes.root}>
       {event && <MalshabToolbar event={event} recording />}
       <div style={{ display: 'flex', justifyContent: 'center', height: '25rem' }}>
-        <img src='videoconference.png' alt='' />
+        <iframe title='videochat' src='https://radar-interviews.azurewebsites.net/?groupId=c5dcdd10-cfad-11eb-a100-cd76ace8e181' width='100%' allow='camera;microphone' style={{ border: 'none' }} />
       </div>
       {malshab && <MalshabDetails malshab={malshab} />}
     </DashboardCard>
