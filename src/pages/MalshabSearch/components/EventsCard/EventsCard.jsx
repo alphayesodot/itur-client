3398c40708e6a9d8230204323b82f237a26854c8
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-06-24 15:29:30 +0300

    feat: Add malshabId param

--- src/pages/MalshabSearch/components/EventsCard/EventsCard.jsx
+++ src/pages/MalshabSearch/components/EventsCard/EventsCard.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import { Typography } from '@material-ui/core';
 import DashboardCard from '../../../../common/DashboardCard/DashboardCard';
+import EventService from '../../../../services/event.service';
 import useStyles from './EventsCard.styles';
 
 const EventsCard = ({ malshabId }) => {
@@ -10,7 +12,11 @@ const EventsCard = ({ malshabId }) => {
   const { t } = useTranslation();
 
   useEffect(() => {
-
+    EventService.getEvents({ malshabId }).then((res) => {
+      setEvents(res);
+    }).catch(() => {
+      toast(t('error.server'));
+    });
   }, []);
 
   return (
@@ -19,7 +25,7 @@ const EventsCard = ({ malshabId }) => {
         {t('title.interviewsHistory')}
       </Typography>
       <DashboardCard className={classes.card}>
-        events
+        {JSON.stringify(events)}
       </DashboardCard>
     </div>
   );
