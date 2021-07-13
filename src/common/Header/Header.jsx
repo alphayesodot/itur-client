dde245983ff2e09f220898063d05ff0845db6d6e
Carmel Tsadok <carmelcarmel88888@gmail.com>
2021-07-13 21:16:11 +0300

    feat: Add roles select

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dotenv from 'dotenv';
 import { useTranslation } from 'react-i18next';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Link } from 'react-router-dom';
 import { Toolbar, AppBar, IconButton, Button } from '@material-ui/core';
+import GenericSelect from '../GenericSelect/GenericSelect';
 import logo from '../../utils/images/logo.svg';
+import UserStore from '../../stores/User.store';
+import { Role } from '../../services/user.service';
 import useStyles from './Header.styles';
+
+dotenv.config();
 
 const Header = () => {
   const classes = useStyles();
+  const [selectedRole, setSelectedRole] = useState({
+    id: UserStore.userProfile.role,
+    label: UserStore.userProfile.role,
+  });
   const date = new Date();
   const { t } = useTranslation();
+
   const getBlessing = (hour) => {
     const hoursRange = { morning: 5, afternoon: 12, evening: 17, night: 21 };
     if (hour >= hoursRange.morning && hour < hoursRange.afternoon) {
@@ -24,6 +35,7 @@ const Header = () => {
     }
     return t('headerBlessings.night');
   };
+
   return (
     <AppBar position='static' className={classes.root}>
       <Toolbar className={classes.toolbar}>
@@ -45,6 +57,14 @@ const Header = () => {
           >
             <AccountCircleIcon className={classes.secondary} />
           </IconButton>
+          {process.env.NODE_ENV === 'development' && (
+            <GenericSelect
+              selectClassName={classes.select}
+              options={Object.values(Role).map((role) => ({ id: role, label: role }))}
+              selectedValue={selectedRole}
+              setSelectedValue={setSelectedRole}
+            />
+          )}
         </div>
         <Link to='/'>
           <Button>
