adc4037f02c8eb4607e26a765e1626d082b4eaa4
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-07-15 10:34:26 +0300

    fix: Fix cr

--- mockserver/authentication.js
+++ mockserver/authentication.js
@@ -2,12 +2,9 @@ import jwt from 'jsonwebtoken';
 import config from './config.js';
 import users from './userdata.js';
 
-const buildJwt = (userId) => {
-  const payload = users[userId];
-  return jwt.sign(payload, config.secret, {
-    algorithm: 'HS256',
-    expiresIn: '1y',
-  });
-};
+const buildJwt = (userId) => jwt.sign(users[userId], config.secret, {
+  algorithm: 'HS256',
+  expiresIn: '1y',
+});
 
 export default buildJwt;
