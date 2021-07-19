23fedb4b8e556f8a9e3b53931b1c34240ce2a772
Yael <lustigyael10@gmail.com>
2021-07-19 11:52:00 +0300

    add file types

--- mockserver/config.js
+++ mockserver/config.js
@@ -4,9 +4,12 @@ const config = {
   tokenName: 'itur',
   secret: 'remember2replace',
   fileUpload: {
-    sizeLimit: 10000000, // bytes
+    sizeLimit: 5 * 10000000, // bytes
     acceptedFileTypes:
-      '.csv, text/csv, text/vnd.ms-excel, application/vnd.ms-excel,  text/xlsx, application/xlsx, application/xml, text/xml, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      '.csv, text/csv, text/vnd.ms-excel, application/vnd.ms-excel,  text/xlsx, application/xlsx,'
+      + 'application/xml, text/xml, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      + 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+      + 'application/pdf,image/png,image/jpeg,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   },
   attachments: {
     acceptedFileTypes: 'application/pdf,image/png,image/jpeg,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
